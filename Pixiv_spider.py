de7faa284cdf2236aa0da54d26793bc27177ba1b
pwjworks <13610513466@163.com>
2018-07-16 01:39:12 +0800

    实现了多个功能
    可以判断多张或一张图片并分别下载

--- Pixiv_spider.py
+++ Pixiv_spider.py
@@ -1,79 +1,206 @@
+#!/usr/bin/python
+import time
 import re
 from queue import Queue
 
+import os
 import requests
 from bs4 import BeautifulSoup
+
+"""
+login
+log
+timer
+"""
 
 
 class spider(object):
     def __init__(self):
-        self.bookmark_url = "https://www.pixiv.net/bookmark.php"
+        """
+        初始化
+        """
+        self.bookmark_url = "https://www.pixiv.net/bookmark.php?rest=show&p=1"
         self.login_api = "https://accounts.pixiv.net/api/login?lang=zh"
         self.login_url = "https://accounts.pixiv.net/login"
         self.session = requests.session()
         self.headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3"
+            "user-agent": "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 "
+                          "Safari/536.3 "
         }
         self.pixiv_id = ""
         self.password = ""
-        self.original_url = Queue()
-        self.num=1
+        self.url_dict_queue = Queue()
+        self.date = time.strftime("%Y%b", time.localtime())
+        self.page_num = 1
+        self.total_pic_num = 0
 
-    def get_PostId(self):
+    def get_postkey(self):
+        """
+        获取登录页面的postkey
+        :return:
+        """
         resp = self.session.get(self.login_url, headers=self.headers)
-        matchObj = re.search('"pixivAccount.postKey":"(\w*?)"', resp.text, re.S)
+        matchObj = re.search('"pixivAccount.postKey":"(\w*?)"', resp.text, re.S)  # 正则匹配postkey
         return matchObj.group(1)
 
     def login(self):
+        """
+        构建headers和formData，把headers和formData用post的方式发送到pixiv的登录api
+        :return: 返回是否登录成功的布尔值
+        """
         headers = self.headers
-        post_key = self.get_PostId()
+        post_key = self.get_postkey()
         headers.update({
             "dnt": "1",
             "origin": "https://accounts.pixiv.net",
             "referer": "https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index"})
-        formData = {
+        form_data = {
             "post_key": post_key,
             "pixiv_id": self.pixiv_id,
             "password": self.password,
             "ref": "wwwtop_accounts_index",
             "return_to": "https: // www.pixiv.net /"
         }
-        resp = self.session.post(self.login_api, data=formData, headers=headers)
+        resp = self.session.post(self.login_api, data=form_data, headers=headers)
+        resp_dict = resp.json()  # 返回字典
+        if "success" in resp_dict["body"]:
+            print("登录成功")
+            return True
+        else:
+            print("登录失败,请检查账号密码")
+            return False
 
     def check_login(self):
         resp = self.session.get(self.login_url, headers=self.headers)
         print(resp.text)
 
     def get_urls(self):
+        """
+        以get的方式获取收藏页面
+        用beautifulSoup获取图片链接
+        将获取的图片id，原始url，图片页面url存进字典队列中
+        :return:
+        """
+        print(self.bookmark_url)
         resp = self.session.get(self.bookmark_url)
+        print(resp.status_code)
         soup = BeautifulSoup(resp.text, "html.parser")
-        resultSet = soup.find_all("a", attrs={"class": "work _work "})
+        result_set = soup.find_all("a", attrs={"class": re.compile("work _work")})  # 抓取图片浏览页面url
 
-        for tag in resultSet:
+        for tag in result_set:
+            num = 0
+            referer_url = "https://www.pixiv.net/" + tag.get("href")
             url = tag.img.get("data-src")
-            original_url = url.replace("c/150x150/img-master", "img-original").replace("_master1200", "")
-            self.original_url.put(original_url)
-            print(original_url)
+            if tag.span is not None:
+                num = int(tag.span.string)  # 取得图片的张数
+            pic_id = re.search("id=(\d*)", referer_url, re.S).group(1)  # 正则匹配图片id
+            original_url = url.replace("c/150x150/img-master", "img-original").replace("_master1200", "")  # 构建图片原始url
+            url_dict = {  # url字典，用于构建request
+                "num": num,
+                "pic_id": pic_id,
+                "original_url": original_url,
+                "referer_url": referer_url
+            }
+            self.url_dict_queue.put(url_dict)
+            self.total_pic_num += 1  # 计数器，计算一共多少张图片
+
+        if self.has_next_page(soup):
+            self.page_num += 1
+            self.bookmark_url = re.sub("(?<=show&p=)\d+", str(self.page_num), self.bookmark_url)
+            self.get_urls()
+        else:
+            print(self.total_pic_num)
 
     def download(self):
-        # while not self.original_url.empty():
+        """
+        取出url字典并进行下载
+        :return:
+        """
+        while not self.url_dict_queue.empty():
+
             try:
-                img_url=self.original_url.get()
-                resp= self.session.get("https://i.pximg.net/img-original/img/2018/06/15/01/19/53/69210112_p0.png")
-                if resp.status_code==403:
-                    new_img_url=img_url.replace(".jpg",".png")
-
-
-
-                # with open(str(self.num)+".png","wb") as f:
-                #     f.write(resp.content)
-                # print(resp.content)
+                url_dict = self.url_dict_queue.get()
+                referer_url = url_dict["referer_url"]
+                original_url = url_dict["original_url"]
+                pic_id = url_dict["pic_id"]
+                num = url_dict["num"]
+                if url_dict["num"] != 0:
+                    print("id=" + url_dict["pic_id"] + "有多张图片")
+                    for i in range(0, num):
+                        num = str(i)
+                        original_url = re.sub("\d(?=.jpg)", num, original_url)
+                        self.download_single_pic(referer_url, original_url, pic_id + "_p" + num)
+                else:
+                    self.download_single_pic(referer_url, original_url, pic_id)
             except Exception as e:
                 print(e)
 
+    def download_single_pic(self, referer_url, original_url, pic_id):
+        """
+        下载单张图片
+        对png和jpg进行判断
+        :param referer_url: 图片预览页面url
+        :param original_url: 图片原始页面url
+        :param pic_id: 图片id
+        :return:
+        """
+        try:
+            headers = {
+                "user-agent": "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 "
+                              "Safari/536.3",
+                "Referer": referer_url,
+                "DNT": "1"
+            }
+            resp = self.session.get(original_url,
+                                    headers=headers)
+            if resp.status_code != 200:
+                new_img_url = original_url.replace(".jpg", ".png")
+                resp = self.session.get(new_img_url, headers=headers)
+                if resp.status_code != 200:
+                    print("发生错误，图片id=" + pic_id)
+                else:
+                    print("获取成功，开始下载id=" + pic_id + "图片")
+                    with open(self.date + "/" + pic_id + ".png", "wb") as f:
+                        f.write(resp.content)
+            else:
+                print("获取成功，开始下载id=" + pic_id + "图片")
+                with open(self.date + "/" + pic_id + ".jpg", "wb") as f:
+                    f.write(resp.content)
+        except Exception as e:
+            print(e)
+
+    def makdir_by_date(self):
+        """
+        按月份创建文件夹，不能创建成功就把图片下载到主程序所在文件夹里
+        :return:
+        """
+        try:
+            if not os.path.exists(self.date):
+                os.makedirs(self.date)
+                print("创建名为\"" + self.date + "\"的文件夹")
+            else:
+                print("已经存在名为\"" + self.date + "\"的文件夹")
+        except Exception as e:
+            print(e)
+            self.date = ""
+
+    def has_next_page(self, soup):
+        """
+        判断是否有下一页
+        :param soup: 分析图片预览页面的soup对象
+        :return: 返回有无下一页的布尔值
+        """
+        result_set = soup.find_all("a", attrs={"rel": "next"})
+        if len(result_set) == 0:
+            return False
+        else:
+            return True
+
 
 if __name__ == '__main__':
-    spider=spider()
+    spider = spider()
     spider.login()
-    # spider.get_urls()
+    spider.get_urls()
+    spider.makdir_by_date()
     spider.download()
+    # spider.test()
